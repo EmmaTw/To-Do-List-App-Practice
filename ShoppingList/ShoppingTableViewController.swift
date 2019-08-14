55ffac9914dee68bb0778bba3fca7509d7feb487
Wu Wan Lun <wuwanlun@Wude-MacBook-Pro.local>
2019-08-14 11:54:26 +0800

    20190814

--- ShoppingList/ShoppingTableViewController.swift
+++ ShoppingList/ShoppingTableViewController.swift
@@ -9,82 +9,105 @@
 import UIKit
 
 class ShoppingTableViewController: UITableViewController {
-
+    
+    var shoppingItems = [String]()
+    
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+        popUpAlertWithDefault(nil, withCompletionHandler: {
+            (success:Bool, result:String?) in
+            if success == true {
+                if let okResult = result {
+                    self.shoppingItems.append(okResult)
+                    //tableView.reloadData()
+                    let insertInfoAtThisIndexPath = IndexPath(row: self.shoppingItems.count-1, section: 0)
+                    self.tableView.insertRows(at: [insertInfoAtThisIndexPath], with: .automatic)
+                    self.saveList()
+                }
+            }
+        })
+    }
+    
+    func saveList() {
+        UserDefaults.standard.set(shoppingItems, forKey: "list")
+    }
+        
+    func loadList() {
+        if let okList = UserDefaults.standard.stringArray(forKey: "list"){
+            shoppingItems = okList
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        
+        popUpAlertWithDefault(shoppingItems[indexPath.row], withCompletionHandler:  {
+            (success:Bool, result:String?) in
+            if success == true {
+                if let okResult = result {
+                    self.shoppingItems[indexPath.row] = okResult
+                    self.tableView.reloadData()
+                    self.saveList()
+                }
+            }
+        })
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+           shoppingItems.remove(at: indexPath.row)
+           saveList()
+           tableView.reloadData()
+        }
+    }
+    
+    func popUpAlertWithDefault(_ defaultValue: String?,withCompletionHandler handler: @escaping (Bool, String?) -> ()) {
+    let alert = UIAlertController(title: "What do you want to do?", message: nil, preferredStyle: .alert)
+        alert.addTextField(configurationHandler: {
+            (textfield) in
+            textfield.placeholder = "Add here"
+            textfield.text = defaultValue
+        })
+       let okAction = UIAlertAction(title: "OK", style: .default, handler: {
+            (action) in
+            //What to do after pressing ok button
+            //Take out text from textfield
+            if let inputText = alert.textFields?[0].text {
+            if inputText != "" {
+                //... Append inputText to shopping list
+                handler(true,inputText)
+            }else{
+                handler(false,nil)
+                }
+            }
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: {
+            (action) in
+            handler(false,nil)
+        })
+    alert.addAction(okAction)
+    alert.addAction(cancelAction)
+    present(alert, animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    loadList()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return shoppingItems.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = shoppingItems[indexPath.row]
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
